dd9cd620dc629120511513782b8f9c868a6ef21f
Lev2114 <lev.shchebletsov@gmail.com>
2024-11-10 18:26:54 +0300

    fin upd????

--- NonlinearEquationSolver.cpp
+++ NonlinearEquationSolver.cpp
@@ -50,7 +50,7 @@ void PrintAnswer(double x, int iterations, int precicion, NonLinearEquationSolve
     if (!answer.xValid) {
         std::cerr << "Решение с данными параметрами данным методом не сходится или сходится слишком медленно! Пожалуйста, попробуйте другой метод!\n";
     } else {
-        std::cout << "Ответ: " << std::setprecision(precicion) << x << std::endl << "Количество итераций: " << iterations << std::endl;;
+        std::cout << "Ответ: " << std::setprecision(precicion) << x << std::endl << "Количество итераций: " << iterations << std::endl;
     }
 }
 
