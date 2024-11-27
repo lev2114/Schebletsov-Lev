c945436c4307de9c9843637cc1f8bb064b83cf5d
Lev2114 <lev.shchebletsov@gmail.com>
2024-11-27 22:41:02 +0300

    add data collection

--- Crypter.cpp
+++ Crypter.cpp
@@ -1,9 +1,12 @@
+#include "Crypter.hpp"
+
 #include <cstring>
 #include <fstream>
 #include <iostream>
 
 namespace {
 const int kArrayAmplify = 2;
+const int kMaxCharNumber = 256;
 const int kStandartCodesLen = 100;
 const int kStandartBufferLen = 2048;
 const int kFilesNumber = 5;
@@ -21,6 +24,44 @@ int* ResizeIntArray(int* array, int& masLen) {
     masLen = newLen;
 
     return newArray;
+}
+
+bool AddIfUnique(int*& array, int& arrayLen, int value) {
+    for (int i = 0; i < arrayLen; ++i) {
+        if (array[i] == value) {
+            return false;
+        }
+    }
+
+    int* newArray = new int[arrayLen + 1];
+    for (int i = 0; i < arrayLen; ++i) {
+        newArray[i] = array[i];
+    }
+    newArray[arrayLen] = value;
+
+    delete[] array;
+    array = newArray;
+    ++arrayLen;
+
+    return true;
+}
+
+bool ContainsChar(char* symbols, int length, char target) {
+    for (int i = 0; i < length; ++i) {
+        if (symbols[i] == target) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool ContainsInt(int* array, int length, int target) {
+    for (int i = 0; i < length; ++i) {
+        if (array[i] == target) {
+            return true;
+        }
+    }
+    return false;
 }
 
 bool CheckFiles(const char* inputFile, const char* codebookFile, const char* encodedFile, const char* decodedFile) {
@@ -54,17 +95,38 @@ bool CheckFiles(const char* inputFile, const char* codebookFile, const char* enc
     return true;
 }
 
-void Encode(const char* inputFile, int* codes, const char* encodedFile, int& numberOfCodes) {
+void Encode(const char* inputFile, int* codes, const char* encodedFile, int& numberOfCodes, Crypter::Data& data) {
     std::ifstream input(inputFile, std::ios::binary | std::ios::in);
     std::ofstream encoded(encodedFile, std::ios::binary | std::ios::out);
     char symbol{};
     int i{};
+    int charCount{};
+
+    data.symbols = new char[kMaxCharNumber];
+    data.symbolData = new Crypter::SymbolData[kMaxCharNumber];
+
     while (input.get(symbol)) {
         if (i == numberOfCodes) {
             i -= numberOfCodes;
         }
+        if (!ContainsChar(data.symbols, kMaxCharNumber, symbol)){
+            data.symbols[charCount] = symbol;
+            data.codes[charCount] = static_cast<int>(symbol);
+            data.symbolData[charCount].usageNumber = 0;
+            charCount += 1;
+        }
+
+        int index = 0;
+        while (data.symbols[index] != symbol) {
+            ++index;
+        }
+
         char encodedSymbol = symbol + static_cast<char>(codes[i]);  // NOLINT
         encoded.put(encodedSymbol);
+
+        data.symbolData[index].usageNumber += 1;
+        AddIfUnique(data.symbolData[index].differentCrypts, data.symbolData[index].usageNumber, encodedSymbol);
+
         ++i;
     }
     input.close();
@@ -141,13 +203,20 @@ void StartProgram(int argc, char** argv) {
     }
 
     int numberOfCodes{};
+
+    Data data{};
+
     int* codes = GetCodes(codebookFile, numberOfCodes);
 
-    Encode(inputFile, codes, encodedFile, numberOfCodes);
+    data.wordsCount = numberOfCodes;
+
+    Encode(inputFile, codes, encodedFile, numberOfCodes, data);
 
     Decode(encodedFile, decodedFile, codes, numberOfCodes);
 
     delete[] codes;
+    delete[] data.symbols;
+    delete[] data.symbolData;
 }
 
 }  // namespace Crypter
--- Crypter.hpp
+++ Crypter.hpp
@@ -2,7 +2,21 @@
 #define CRYPTER
 
 namespace Crypter {
+
+struct SymbolData{
+    int usageNumber{};
+    int* differentCrypts = nullptr;
+};
+
+struct Data {
+    char* symbols = nullptr;
+    int* codes = nullptr;
+    SymbolData* symbolData = nullptr;
+    int wordsCount{};
+    int textLen{};
+};
+
 void StartProgram(int argc, char** argv);
-}
+}  // namespace Crypter
 
 #endif  // DEFINE CRYPTER
